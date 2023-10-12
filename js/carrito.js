30151655b75ec70d00d60bd9d10482a92d34ba5d
Alejo Calderon <santiago.alejo.calderon@gmail.com>
2023-10-12 19:30:35 -0300

    se agrega buscador. mas peliculas. sweetalerts

--- js/carrito.js
+++ js/carrito.js
@@ -1,34 +1,13 @@
-
-const peliculas = [
-    {
-        id: 1,
-        imagen: "./img/avengers1.jpg",
-        nombre: "AVENGERS",
-        precio: "AR$ 800"
-    },
-    {
-        id: 2,
-        imagen: "./img/avengers2.jpg",
-        nombre: "AVENGERS - AGE OF ULTRON",
-        precio: "AR$ 1200"
-    },
-    {
-        id: 3,
-        imagen: "./img/avengers3.jpg",
-        nombre: "AVENGERS - INFINITY WAR",
-        precio: "AR$ 2000"
-    },
-    {
-        id: 4,
-        imagen: "./img/avengers4.jpg",
-        nombre: "AVENGERS - END GAME",
-        precio: "AR$ 3000"
-    },
-]
-
 const carritoArray = JSON.parse(localStorage.getItem("carritoStorage")) || []
+const buscador = document.getElementById("buscador")
 
-function renderPeliculas() {
+async function traerProductos() {
+    const resp = await fetch("./peliculas.json")
+    const peliculas = await resp.json()
+    renderPeliculas(peliculas)
+}
+
+function renderPeliculas(peliculas) {
     const divPeliculas = document.querySelector("#contenedorDePelis")
     divPeliculas.innerHTML = ""
     peliculas.forEach((pelicula) => {
@@ -37,19 +16,30 @@ function renderPeliculas() {
         contenedorPelicula.innerHTML = `
             <img src="${pelicula.imagen}">
             <p>${pelicula.nombre}</p>
-            <p>${pelicula.precio}</p>
-            <button class="slider__link" onclick="peliAgregada(${pelicula.id})">+</button>
+            <p>AR$ ${pelicula.precio}</p>
+            <button class="slider__link" id="${pelicula.id}">+</button>
             `
         divPeliculas.appendChild(contenedorPelicula)
+        const btnAgregar = document.getElementById(`${pelicula.id}`)
+        btnAgregar.addEventListener("click", () => {
+            peliAgregada(peliculas, pelicula.id)
+        })
     })
 }
 
 
 
-function peliAgregada(id) {
+function peliAgregada(peliculas, id) {
     const peliculaSeleccionada = peliculas.find(pelicula => pelicula.id === id)
     carritoArray.push(peliculaSeleccionada)
     localStorage.setItem("carritoStorage", JSON.stringify(carritoArray))
+
+    Swal.fire({
+        icon: 'success',
+        text: 'Producto agregado al carrito',
+        showConfirmButton: false,
+        timer: 1500
+    })
     renderCarrito()
 }
 
@@ -63,27 +53,29 @@ function renderCarrito() {
         carritoDiv.innerHTML = `
         <img src="${pelicula.imagen}">
         <p>${pelicula.nombre}</p>
-        <p>${pelicula.precio}</p>
+        <p>AR$ ${pelicula.precio}</p>
         <button class="btn btn-danger" onclick="eliminarPelicula(${index})">+</button>
         `
         carrito.appendChild(carritoDiv)
+
     })
     totalCarrito()
 }
 
 function totalCarrito() {
     let total = 0
-    const totalCarrito = document.querySelector("#total")
+    const totalCarrito = document.getElementById("total")
 
     carritoArray.forEach((pelicula) => {
-        const preciosPelicula = Number(pelicula.precio.replace("AR$", ""))
-        total += preciosPelicula
+        const preciosPelicula = Number(pelicula.precio)
+        total = total + preciosPelicula
 
-        totalCarrito.innerHTML = `
+
+    })
+    totalCarrito.innerHTML = `
         <p>Total AR$ ${total}</p>
         <button class="btn__comprar" onclick="realizarCompra()">Comprar</button>
         `
-    })
 }
 
 function eliminarPelicula(index) {
@@ -96,12 +88,45 @@ function eliminarPelicula(index) {
 function realizarCompra() {
     localStorage.removeItem("carritoStorage")
     carritoArray.length = 0
+    Swal.fire({
+        icon: 'success',
+        title: 'Su pedido fue procesado',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+    })
     renderCarrito()
     totalCarrito()
 }
 
+buscador.addEventListener(("input"), () => {
+    const inputValue = buscador.value.toUpperCase()
+    if (inputValue == "") {
+        traerProductos()
+    } else {
+        fetch("./peliculas.json")
+            .then(resp => resp.json())
+            .then(peliculas => {
+                const pelisFiltradas = peliculas.filter(pelicula => pelicula.nombre.includes(inputValue))
+                if (pelisFiltradas.length > 0) {
+                    renderPeliculas(pelisFiltradas)
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'La pelicula no existe',
+                    })
+                }
+            })
+    }
+})
+
 document.addEventListener("DOMContentLoaded", () => {
-    renderPeliculas()
+    traerProductos()
     renderCarrito()
 })
+
 
